c29584145755953ab0917978bc59b0a01d1c93cd
Oooooomin2 <ohmine0704@gmail.com>
2023-08-11 18:24:30 +0900

    不要なクラスを削除

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using OpenAI_API;
-using System.Net.Http;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace ConsoleApp1
 {
@@ -26,41 +22,5 @@ namespace ConsoleApp1
                 Console.WriteLine($"AIヤンキー: {response}");
             }
         }
-    }
-
-    class RequestBody
-    {
-        [JsonPropertyName("model")]
-        public string Model { get; set; }
-        [JsonPropertyName("messages")]
-        public List<Messages> Messages { get; set; }
-    }
-
-    class Messages
-    {
-        [JsonPropertyName("role")]
-        public string Role { get; set; }
-        [JsonPropertyName("content")]
-        public string Content { get; set; }
-    }
-
-    class Response
-    {
-        [JsonPropertyName("choices")]
-        public Choices[] Choices { get; set; }
-    }
-
-    class Choices
-    {
-        [JsonPropertyName("message")]
-        public Message Message { get; set; }
-    }
-
-    class Message
-    {
-        [JsonPropertyName("role")]
-        public string Role { get; set; }
-        [JsonPropertyName("content")]
-        public string Content { get; set; }
     }
 }
